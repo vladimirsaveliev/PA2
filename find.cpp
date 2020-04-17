dde8aac1e2c663ef363f7c21420531c8f90364e5
Vladimir Saveliev <c17830@cray.com>
2020-04-17 12:47:48 +0300

    changed demo to use vector<T>

--- find.cpp
+++ find.cpp
@@ -9,18 +9,38 @@
 
 using namespace std;
 
+class T {
+private:
+	string m_Name;
+	string m_SurName;
+public:
+	T(const char *name,  const char *surname) :m_Name(name), m_SurName(surname) {}
+	bool operator ==(const T& t2);
+	friend ostream & operator << (ostream &os, const T & t);
+};
+
+bool T::operator ==(const T& t2)
+{
+	return m_Name == t2.m_Name && m_SurName == t2.m_SurName;
+}
+
+ostream & operator << (ostream &os, const T & t)
+{
+	os << t.m_Name << " " << t.m_SurName;
+	return os;
+}
+
 int main()
 {
-	string str;
-	vector <string> vec;
+	vector <T> vec;
 
-	vec.push_back("vasya");
-	vec.push_back("petya");
-	vec.push_back("john");
+	vec.push_back(T("vasya", "ivanov"));
+	vec.push_back(T("petya", "petrov"));
+	vec.push_back(T("harry", "truman"));
 
-	vector<string>::const_iterator it;
+	vector<T>::const_iterator it;
 
-	it = find(vec.begin(), vec.end(), "john");
+	it = find(vec.begin(), vec.end(), T("harry", "truman"));
 	if (it == vec.end())
 		cout << "not found" << endl;
 	else
